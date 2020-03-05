b936237c17f5cf6207e4dd76c40e88a5e98b3974
juliebulakh <bulakh.julie@gmail.com>
2020-03-05 23:31:37 +0300

    feat: add solution

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,5 +1,22 @@
-module.exports = function repeater(/* str, options */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+  let repeatTimes = options.repeatTimes || 1;
+  let separator = String(options.separator) || "+";
+  let addition = String(options.addition) || "";
+  let additionSeparator = String(options.additionSeparator) || "|";
+  let additionRepeatTimes = options.additionRepeatTimes || 1;
+
+  let strAdd = [];
+  let strRep = [];
+
+  for (i = 1; i < additionRepeatTimes + 1; i++) {
+    strAdd = strAdd.concat(addition);
+  }
+  if (options.additionSeparator) {
+    strAdd = strAdd.join(additionSeparator);
+  }
+    else strAdd = strAdd.join("");
+    
+  for (i = 1; i < repeatTimes + 1; i++) strRep = strRep.concat(str + strAdd);
+
+  return strRep.join(separator);
 };
-  
