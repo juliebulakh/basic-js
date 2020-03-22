67ca50b68e53c3302f008bcca1f6352e02654411
juliebulakh <bulakh.julie@gmail.com>
2020-03-22 23:24:22 +0300

    add solution

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,67 @@
-class VigenereCipheringMachine {
-    encrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
-    }
 
-    decrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+class VigenereCipheringMachine {
+    constructor(accept) {
+      // console.log(accept);
+  
     }
-}
+    encrypt(message, key) {
+        let alpha = [];
+        let keyA = key.repeat(10);
+        for (let i = 65; i < 91; i++) {
+            alpha.push(String.fromCharCode(i));
+        }
+      let arr = [];
+      for (let i = 0; i < message.length; i++) {
+          if (alpha.includes(message.toUpperCase().charAt(i))) {
+              arr.push((alpha.indexOf(message.toUpperCase().charAt(i)) + alpha.indexOf(keyA.toUpperCase().charAt(i))) % alpha.length);
+          }
+          else {
+              arr.push(message.toUpperCase().charAt(i));
+          }
+        }
+    
+      let final = [];
+      for (let i = 0; i < message.length; i++) {
+        if (alpha.includes(alpha[arr[i]])) {
+          final.push(alpha[arr[i]]);
+        }
+        else final.push(arr[i]);
+        // final.push(alpha[arr[i]]);
+        }
+   
+      return final.join("");
+      // return arr;
+  
+    }
+    decrypt(encryptedMessage, key) {
+        let alpha = [];
+        let keyA = key.repeat(10);
+        for (let i = 65; i < 91; i++) {
+            alpha.push(String.fromCharCode(i));
+        }
+      let arr = [];
+      for (let i = 0; i < message.length; i++) {
+          if (alpha.includes(message.toUpperCase().charAt(i))) {
+              arr.push((alpha.indexOf(message.toUpperCase().charAt(i)) - alpha.indexOf(keyA.toUpperCase().charAt(i))) % alpha.length);
+          }
+          else {
+              arr.push(message.toUpperCase().charAt(i));
+          }
+        }
+    
+      let final = [];
+      for (let i = 0; i < message.length; i++) {
+        if (alpha.includes(alpha[arr[i]])) {
+          final.push(alpha[arr[i]]);
+        }
+        else final.push(arr[i]);
+        // final.push(alpha[arr[i]]);
+        }
+   
+      return final.join("");
+      // return arr;
+
+    }
+  }
 
 module.exports = VigenereCipheringMachine;
